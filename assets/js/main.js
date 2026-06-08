f6cb5626bda872e06839868b43d157441742ad58
sakae .s <user@s3110MacBook-Air.local>
2026-06-08 11:01:14 +0900

    faq実装中

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,3 +80,138 @@ if (mvSwiperEl && typeof Swiper !== 'undefined') {
     }
   });
 }
+
+
+
+// ====================
+//  アコーディオン
+// ====================
+
+document.addEventListener('DOMContentLoaded', () => {
+  setUpAccordion();
+});
+
+const setUpAccordion = () => {
+  const details = document.querySelectorAll('.js-details');
+  const IS_OPENED_CLASS = 'is-opened';
+
+  details.forEach((element) => {
+    const summary = element.querySelector('.js-summary');
+    const content = element.querySelector('.js-content');
+
+    const toggleAccordion = (event) => {
+      event.preventDefault();
+
+      if (element.classList.contains(IS_OPENED_CLASS)) {
+        element.classList.toggle(IS_OPENED_CLASS);
+        closingAnim(content, element).restart();
+      } else {
+        element.classList.toggle(IS_OPENED_CLASS);
+        element.setAttribute('open', 'true');
+        openingAnim(content).restart();
+      }
+    };
+
+    summary.addEventListener('click', toggleAccordion);
+    content.addEventListener('click', toggleAccordion);
+});
+}
+/**
+ * アコーディオンを閉じる時のアニメーション
+ */
+const closingAnim = (content, element) =>
+  gsap.to(content, {
+    height: 0,
+    opacity: 0,
+    duration: 0.4,
+    ease: 'power3.out',
+    overwrite: true,
+    onComplete: () => {
+      // アニメーションの完了後にopen属性を取り除く
+      element.removeAttribute('open');
+    },
+  });
+
+/**
+ * アコーディオンを開く時のアニメーション
+ */
+const openingAnim = (content) =>
+  gsap.fromTo(
+    content,
+    {
+      height: 0,
+      opacity: 0,
+    },
+    {
+      height: content.scrollHeight,
+      opacity: 1,
+      duration: 0.4,
+      ease: 'power3.out',
+      overwrite: true,
+      onComplete: () => {
+        content.style.height = "auto";
+      }
+    }
+  );
+
+
+
+// ====================
+//  パララックス
+// ====================
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 769px)", () => {
+
+  gsap.utils.toArray('.js-parallax1').forEach(wrap => {
+    gsap.to(wrap, {
+      y: -300,
+      scrollTrigger: {
+        trigger: wrap,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.5
+      }
+    })
+  });
+  gsap.utils.toArray('.js-parallax2').forEach(wrap => {
+    gsap.to(wrap, {
+      y: -300,
+      scrollTrigger: {
+        trigger: wrap,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.5
+      }
+    })
+  });
+
+});
+
+mm.add("(max-width: 768px)", () => {
+
+  gsap.utils.toArray('.js-parallax1').forEach(wrap => {
+    gsap.to(wrap, {
+      y: -150,
+      scrollTrigger: {
+        trigger: wrap,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.5
+      }
+    })
+  });
+  gsap.utils.toArray('.js-parallax2').forEach(wrap => {
+    gsap.to(wrap, {
+      y: -150,
+      scrollTrigger: {
+        trigger: wrap,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.5
+      }
+    })
+  });
+
+});
