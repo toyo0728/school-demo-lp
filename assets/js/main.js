560c3a7605d4159e63aa5c9f714c89f241fb085d
sho <sho910.0716@gmail.com>
2026-06-17 21:13:13 +0900

    feat: missionセクション実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,3 +135,35 @@ dialogs.forEach(button => {
     }
   });
 });
+
+// 時間差アニメーション
+window.addEventListener("load", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const timeDelay = 350; // 時間差のタイミング(ミリ秒)
+  const maxItemNumber = 4; // 時間差で発火させる最大要素数
+
+  // fade in
+  for (let i = 0; i < maxItemNumber; i++) {
+    const fadeInItems = document.querySelectorAll(
+      `.animated__fadeIn.--delay${i}`,
+    );
+    fadeInFunction(fadeInItems, i * timeDelay);
+  }
+
+  function fadeInFunction(fadeInItems, timeout) {
+    fadeInItems.forEach((item) => {
+      ScrollTrigger.create({
+        trigger: item,
+        start: "top 70%", // 要素が上部から70%の位置で発火
+        markers: false,
+        onEnter: () => {
+          // 要素内に入ったら、js-showクラスをつける
+          setTimeout(() => {
+            item.classList.add("js-show");
+          }, timeout);
+        },
+      });
+    });
+  }
+});
